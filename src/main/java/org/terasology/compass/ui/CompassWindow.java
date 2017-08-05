858d8f18f88b0b7dfdaaa84a09ef85931c228ec9
TheFlash98 <mastermind.sarthak@gmail.com>
2017-08-05 14:26:36 +0530

    Display the UITextbox with Yaw

--- src/main/java/org/terasology/compass/ui/CompassWindow.java
+++ src/main/java/org/terasology/compass/ui/CompassWindow.java
@@ -18,31 +18,47 @@ package org.terasology.compass.ui;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.logic.location.LocationComponent;
 import org.terasology.logic.players.LocalPlayer;
-import org.terasology.registry.CoreRegistry;
+import org.terasology.math.TeraMath;
+import org.terasology.math.geom.Quat4f;
+import org.terasology.network.ServerInfoMessage;
+import org.terasology.registry.In;
 import org.terasology.rendering.nui.databinding.Binding;
 import org.terasology.rendering.nui.databinding.ReadOnlyBinding;
-import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.rendering.nui.layers.hud.CoreHudWidget;
 import org.terasology.rendering.nui.widgets.UIText;
 
-
+import java.util.concurrent.Future;
 
 
 public class CompassWindow extends CoreHudWidget {
     //private UIText compass;
     private static final Logger logger = LoggerFactory.getLogger(CompassWindow.class);
 
+    @In
+    private LocalPlayer localPlayer;
+
     @Override
-    public void initialise(){
-        UIText compass = find("compass", UIText.class);
+    public void initialise() {
+        UIText compass = find("test", UIText.class);
         compass.bindVisible(new ReadOnlyBinding<Boolean>() {
             @Override
             public Boolean get() {
-                EntityRef character = CoreRegistry.get(LocalPlayer.class).getCharacterEntity();
-                logger.info("This is shit"+Boolean.toString(character != null));
+                EntityRef character = localPlayer.getCharacterEntity();
+                logger.info("This is shit" + (character != null));
                 return character != null;
             }
         });
+        compass.bindText(new ReadOnlyBinding<String>() {
+             @Override
+             public String get() {
+                 EntityRef character = localPlayer.getCharacterEntity();
+                 Quat4f rotation = character.getComponent(LocationComponent.class).getWorldRotation();
+                 return "" + rotation.getYaw()*180/TeraMath.PI;
+             }
+         }
+        );
     }
 }
