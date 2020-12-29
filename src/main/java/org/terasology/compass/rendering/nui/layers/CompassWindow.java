ca098705f1984208f98609e1240d799ea47dbec0
Michael Pollind <mpollind@gmail.com>
2020-12-28 22:02:22 -0800

    feature(JOML): migrate CompassWindow

--- src/main/java/org/terasology/compass/rendering/nui/layers/CompassWindow.java
+++ src/main/java/org/terasology/compass/rendering/nui/layers/CompassWindow.java
@@ -3,23 +3,26 @@
 package org.terasology.compass.rendering.nui.layers;
 
 
+import org.joml.AxisAngle4f;
+import org.joml.Quaternionf;
 import org.joml.Rectanglei;
+import org.joml.Vector2i;
+import org.joml.Vector3f;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.logic.location.LocationComponent;
+import org.terasology.math.Direction;
 import org.terasology.math.JomlUtil;
-import org.terasology.math.geom.Quat4f;
-import org.joml.Vector2i;
-import org.terasology.math.geom.Vector3f;
-import org.terasology.rendering.assets.material.Material;
-import org.terasology.rendering.assets.mesh.Mesh;
-import org.terasology.rendering.assets.texture.Texture;
 import org.terasology.nui.Border;
 import org.terasology.nui.Canvas;
 import org.terasology.nui.CoreWidget;
 import org.terasology.nui.databinding.Binding;
 import org.terasology.nui.databinding.DefaultBinding;
+import org.terasology.nui.util.RectUtility;
+import org.terasology.rendering.assets.material.Material;
+import org.terasology.rendering.assets.mesh.Mesh;
+import org.terasology.rendering.assets.texture.Texture;
 import org.terasology.rendering.nui.CanvasUtility;
 import org.terasology.utilities.Assets;
 
@@ -60,17 +63,23 @@ public class CompassWindow extends CoreWidget {
         int arrowHeight = arrowhead.getHeight() * 2;
         int arrowX = (width - arrowWidth) / 2;
         int arrowY = (height - arrowHeight) / 2;
-        Rectanglei screenArea = JomlUtil.rectangleiFromMinAndSize(arrowX, arrowY, arrowWidth, arrowHeight);
+
+        Rectanglei screenArea = RectUtility.createFromMinAndSize(arrowX, arrowY, arrowWidth, arrowHeight);
 
         // UITexture should be used here, but it doesn't work
         Material material = Assets.getMaterial("engine:UILitMesh").get();
         material.setTexture("texture", arrowhead);
         Mesh mesh = Assets.getMesh("engine:UIBillboard").get();
         // The scaling seems to be completely wrong - 0.8f looks ok
-        Quat4f q = locationComponent.getWorldRotation();
+        Quaternionf q = locationComponent.getWorldRotation(new Quaternionf());
 
-        float rotation = q.getYaw();
-        CanvasUtility.drawMesh(canvas, mesh, material, JomlUtil.from(screenArea), new Quat4f(0, 0, rotation), new Vector3f(0, 0, 0), 0.8f);
+        Vector3f forward = q.transform(Direction.FORWARD.asVector3f(), new Vector3f());
+        Vector3f left = forward.cross(Direction.UP.asVector3f(), new Vector3f());
+        Vector3f orthoForward = left.cross(Direction.UP.asVector3f(), new Vector3f());
+
+        AxisAngle4f ax = new AxisAngle4f();
+        new Quaternionf().rotationTo(Direction.FORWARD.asVector3f(), orthoForward).get(ax);
+        CanvasUtility.drawMesh(canvas, mesh, material, JomlUtil.from(screenArea), JomlUtil.from(new Quaternionf().setAngleAxis(-ax.angle * Math.signum(ax.y) + Math.PI, 0, 0, 1)), JomlUtil.from(new Vector3f(0, 0, 0)), 0.8f);
     }
 
 
@@ -89,5 +98,4 @@ public class CompassWindow extends CoreWidget {
     public Vector2i getPreferredContentSize() {
         return new Vector2i(50, 50);
     }
-
 }
