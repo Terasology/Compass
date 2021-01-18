3f20c74d1443731ed6c2b97afcf2394cc9ce0903
Michael Pollind <mpollind@gmail.com>
2021-01-17 16:04:12 -0800

    feat(JOML): migrate joml to 1.10.0 and 1.0.0-SNAPSHOT for geometry
     PR: https://github.com/MovingBlocks/Terasology/pull/4413

--- src/main/java/org/terasology/compass/rendering/nui/layers/CompassWindow.java
+++ src/main/java/org/terasology/compass/rendering/nui/layers/CompassWindow.java
@@ -3,7 +3,7 @@
 package org.terasology.compass.rendering.nui.layers;
 
 
-import org.joml.Rectanglei;
+import org.terasology.joml.geom.Rectanglei;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.terasology.entitySystem.entity.EntityRef;
